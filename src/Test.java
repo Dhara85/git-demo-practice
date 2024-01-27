d56e91ec36fb3a7b41d630b28751c72e7287ea08
Dhara85 <idharap07@gmail.com>
2024-01-27 15:14:12 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println("My name is Dhara, i m good");
         System.out.println("My name is Dhara, I am automation tester");
     }
 }
