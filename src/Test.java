20efaddfb3e0514dee697cd8a2d90b1657af289a
Dhara85 <146894131+Dhara85@users.noreply.github.com>
2024-01-27 15:09:45 +0000

    commit 1

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My name is Dhara");
+        System.out.println("My name is Dhara, I am automation tester");
     }
 }
